df662bb905db3c0b64d781977340193bf521203c
pyaehein <nnieheayp@yahoo.com>
2016-10-26 01:08:15 +0630

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('ph', function () {
-    return bcrypt('naruton5');
-});
-
 Route::group(['prefix' => 'admin'], function () {
 	 
  
