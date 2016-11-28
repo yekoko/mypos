8c06be8da66fd44fb921cd0e8911ca3a80c09fac
pyaehein <nnieheayp@yahoo.com>
2016-11-28 23:56:47 +0630

    added latitude and longitude

--- app/Job.php
+++ app/Job.php
@@ -8,6 +8,12 @@ class Job extends Model
 {
     protected $table = 'jobs';
 
+    protected $fillable = [
+        'title', 'company_id', 'category_id', 'experience_id',
+        'min_salary', 'max_salary', 'requirements', 'responsibilities',
+        'description', 'email', 'phone_no', 'address', 'end_date'
+    ];
+
     public function category()
     {
     	return $this->belongsTo('App\Category');
